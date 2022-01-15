c5b63cde8d38bc4a25dae3edcad257bef5921dc7
ŞERİF ÇOLAKEL <84101614+serifcolakel@users.noreply.github.com>
2022-01-15 11:49:12 +0300

    download resume button style added.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import "./App.css";
-import React, { useRef } from "react";
-import ReactToPrint from "react-to-print";
-
 import { ComponentToPrint } from "./ComponentToPrint";
 import Form from "./Form";
 
 function App() {
-  const componentRef = useRef();
   return (
     <div>
-      <ReactToPrint
-        trigger={() => <button className="print">Print this out!</button>}
-        content={() => componentRef.current}
-      />
       <Form />
-      <ComponentToPrint ref={componentRef} />
+      <ComponentToPrint />
     </div>
   );
 }
--- src/ComponentToPrint.js
+++ src/ComponentToPrint.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import { BsFillTelephoneFill, BsLinkedin } from "react-icons/bs";
 import { AiFillGithub, AiOutlineGlobal } from "react-icons/ai";
 import { MdLocationPin, MdMail } from "react-icons/md";
@@ -6,10 +6,9 @@ import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
-
+import ReactToPrint from "react-to-print";
 import { Rating, Typography } from "@mui/material";
 import Details from "./Details";
-
 const Item = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
   padding: theme.spacing(1),
@@ -21,284 +20,293 @@ const Item = styled(Paper)(({ theme }) => ({
 }));
 
 export const ComponentToPrint = React.forwardRef((props, ref) => {
+  const componentRef = useRef();
+  const [close, setClose] = React.useState(true);
   return (
-    <div className="App" ref={ref}>
-      <Box sx={{ flexGrow: 1, padding: 1 }}>
-        <Grid container spacing={2}>
-          <Grid item xs={4}>
-            <Item>
-              <img
-                src="https://i.hizliresim.com/5h20dz6.jpg"
-                alt="photo"
-                style={{
-                  width: "180px",
-                  border: "5px solid #ff5722",
-                  borderRadius: "50%",
-                  objectFit: "cover",
-                }}
-              />
-              <Typography
-                variant="h6"
-                color="textPrimary"
-                sx={{
-                  textAlign: "center",
-                  fontWeight: "bold",
-                  color: "#fff",
-
-                  marginTop: "1rem",
-                  fontFamily: "initial",
-                }}
-              >
-                ŞERİF ÇOLAKEL
+    <>
+      <div className="App" ref={componentRef}>
+        <Box sx={{ flexGrow: 1, padding: 1 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={4}>
+              <Item>
+                <img
+                  src="https://i.hizliresim.com/5h20dz6.jpg"
+                  alt="photo"
+                  style={{
+                    width: "180px",
+                    border: "5px solid #ff5722",
+                    borderRadius: "50%",
+                    objectFit: "cover",
+                  }}
+                />
+                <ReactToPrint
+                  trigger={() => (
+                    <button className="btn btn-primary">DOWNLOAD RESUME</button>
+                  )}
+                  content={() => componentRef.current}
+                />
                 <Typography
+                  variant="h6"
                   color="textPrimary"
                   sx={{
                     textAlign: "center",
                     fontWeight: "bold",
                     color: "#fff",
+
                     marginTop: "1rem",
+                    fontFamily: "initial",
                   }}
                 >
-                  ELEKTRİK ve ELEKTRONİK MÜHENDİSİ
+                  ŞERİF ÇOLAKEL
+                  <Typography
+                    color="textPrimary"
+                    sx={{
+                      textAlign: "center",
+                      fontWeight: "bold",
+                      color: "#fff",
+                      marginTop: "1rem",
+                    }}
+                  >
+                    ELEKTRİK ve ELEKTRONİK MÜHENDİSİ
+                  </Typography>
                 </Typography>
-              </Typography>
-
-              <Typography
-                sx={{
-                  textAlign: "left",
-                  backgroundColor: "#232537",
-                  padding: "10px 15px",
-                  fontWeight: "bold",
-                  color: "#fff",
-                  fontSize: "0.8rem",
-                  borderLeft: "5px solid #ff5722",
-                  marginTop: "1rem",
-                  fontFamily: "initial",
-                }}
-              >
-                İLETİŞİM BİLGİLERİ
-              </Typography>
-              <a>
-                <MdLocationPin size={18} color="#fff" />
+                <Typography
+                  sx={{
+                    textAlign: "left",
+                    backgroundColor: "#232537",
+                    padding: "10px 15px",
+                    fontWeight: "bold",
+                    color: "#fff",
+                    fontSize: "0.8rem",
+                    borderLeft: "5px solid #ff5722",
+                    marginTop: "1rem",
+                    fontFamily: "initial",
+                  }}
+                >
+                  İLETİŞİM BİLGİLERİ
+                </Typography>
+                <a>
+                  <MdLocationPin size={18} color="#fff" />
+                  <Typography
+                    sx={{
+                      fontWeight: "bold",
+                      color: "#fff",
+                      fontSize: ".75rem",
+                    }}
+                  >
+                    Antakya / Hatay
+                  </Typography>
+                </a>
+                <a href="mailto:serifcolakel0@gmail.com">
+                  <MdMail size={18} color="#fff" />
+                  <Typography
+                    sx={{
+                      fontWeight: "bold",
+                      color: "#fff",
+                      fontSize: ".75rem",
+                    }}
+                  >
+                    serifcolakel0@gmail.com
+                  </Typography>
+                </a>
+                <a href="tel:+905398568303">
+                  <BsFillTelephoneFill size={18} color="#fff" />
+                  <Typography
+                    sx={{
+                      fontWeight: "bold",
+                      color: "#fff",
+                      fontSize: ".75rem",
+                    }}
+                  >
+                    +90 539 856 83 03
+                  </Typography>
+                </a>
+                <a
+                  href="https://www.linkedin.com/in/serifcolakel/"
+                  target="_blank"
+                >
+                  <BsLinkedin size={18} color="#fff" />
+                  <Typography
+                    sx={{
+                      fontWeight: "bold",
+                      color: "#fff",
+                      fontSize: ".75rem",
+                    }}
+                  >
+                    /serifcolakel
+                  </Typography>
+                </a>
+                <a href="https://serifcolakel.vercel.app/" target="_blank">
+                  <AiOutlineGlobal size={18} color="#fff" />
+                  <Typography
+                    sx={{
+                      fontWeight: "bold",
+                      color: "#fff",
+                      fontSize: ".75rem",
+                    }}
+                  >
+                    https://serifcolakel.vercel.app/
+                  </Typography>
+                </a>
+                <a href="https://github.com/serifcolakel" target="_blank">
+                  <AiFillGithub size={18} color="#fff" />
+                  <Typography
+                    sx={{
+                      fontWeight: "bold",
+                      color: "#fff",
+                      fontSize: ".75rem",
+                    }}
+                  >
+                    /serifcolakel
+                  </Typography>
+                </a>
+                <Typography
+                  sx={{
+                    textAlign: "left",
+                    backgroundColor: "#232537",
+                    padding: "10px 15px",
+                    fontWeight: "bold",
+                    color: "#fff",
+                    fontSize: "0.8rem",
+                    borderLeft: "5px solid #ff5722",
+                    marginTop: "1rem",
+                    fontFamily: "initial",
+                  }}
+                >
+                  REFERANSLAR
+                </Typography>
                 <Typography
                   sx={{
                     fontWeight: "bold",
                     color: "#fff",
+                    textAlign: "center",
                     fontSize: ".75rem",
+                    padding: "15px 3px 5px 3px",
+                    borderBottom: "1px solid #ff5722",
+                    width: "100%",
                   }}
                 >
-                  Antakya / Hatay
+                  Prof. Dr. Abdulkadir ŞENGÜR
                 </Typography>
-              </a>
-              <a href="mailto:serifcolakel0@gmail.com">
-                <MdMail size={18} color="#fff" />
-                <Typography
-                  sx={{
-                    fontWeight: "bold",
-                    color: "#fff",
-                    fontSize: ".75rem",
-                  }}
-                >
-                  serifcolakel0@gmail.com
-                </Typography>
-              </a>
-              <a href="tel:+905398568303">
-                <BsFillTelephoneFill size={18} color="#fff" />
-                <Typography
-                  sx={{
-                    fontWeight: "bold",
-                    color: "#fff",
-                    fontSize: ".75rem",
-                  }}
-                >
-                  +90 539 856 83 03
-                </Typography>
-              </a>
-              <a
-                href="https://www.linkedin.com/in/serifcolakel/"
-                target="_blank"
-              >
-                <BsLinkedin size={18} color="#fff" />
-                <Typography
-                  sx={{
-                    fontWeight: "bold",
-                    color: "#fff",
-                    fontSize: ".75rem",
-                  }}
-                >
-                  /serifcolakel
-                </Typography>
-              </a>
-              <a href="https://serifcolakel.vercel.app/" target="_blank">
-                <AiOutlineGlobal size={18} color="#fff" />
-                <Typography
-                  sx={{
-                    fontWeight: "bold",
-                    color: "#fff",
-                    fontSize: ".75rem",
-                  }}
-                >
-                  https://serifcolakel.vercel.app/
-                </Typography>
-              </a>
-              <a href="https://github.com/serifcolakel" target="_blank">
-                <AiFillGithub size={18} color="#fff" />
-                <Typography
-                  sx={{
-                    fontWeight: "bold",
-                    color: "#fff",
-                    fontSize: ".75rem",
-                  }}
-                >
-                  /serifcolakel
-                </Typography>
-              </a>
-              <Typography
-                sx={{
-                  textAlign: "left",
-                  backgroundColor: "#232537",
-                  padding: "10px 15px",
-                  fontWeight: "bold",
-                  color: "#fff",
-                  fontSize: "0.8rem",
-                  borderLeft: "5px solid #ff5722",
-                  marginTop: "1rem",
-                  fontFamily: "initial",
-                }}
-              >
-                REFERANSLAR
-              </Typography>
-              <Typography
-                sx={{
-                  fontWeight: "bold",
-                  color: "#fff",
-                  textAlign: "center",
-                  fontSize: ".75rem",
-                  padding: "15px 3px 5px 3px",
-                  borderBottom: "1px solid #ff5722",
-                  width: "100%",
-                }}
-              >
-                Prof. Dr. Abdulkadir ŞENGÜR
-              </Typography>
-              <a
-                href="mailto:ksengur@firat.edu.tr
+                <a
+                  href="mailto:ksengur@firat.edu.tr
 "
-              >
-                <MdMail size={18} color="#fff" />
+                >
+                  <MdMail size={18} color="#fff" />
 
+                  <Typography
+                    sx={{
+                      fontWeight: "bold",
+                      color: "#fff",
+                      fontSize: ".75rem",
+                    }}
+                  >
+                    ksengur@firat.edu.tr
+                  </Typography>
+                </a>
+                <a href="tel:04242374343">
+                  <BsFillTelephoneFill size={18} color="#fff" />
+                  <Typography
+                    sx={{
+                      fontWeight: "bold",
+                      color: "#fff",
+                      fontSize: ".75rem",
+                    }}
+                  >
+                    +90 0424 237 43 43
+                  </Typography>
+                </a>
                 <Typography
                   sx={{
                     fontWeight: "bold",
                     color: "#fff",
+                    textAlign: "center",
                     fontSize: ".75rem",
+                    marginTop: "8px",
+                    padding: "5px 3px 5px 3px",
+                    borderBottom: "1px solid #ff5722",
+                    borderTop: "1px solid #ff5722",
+                    width: "100%",
                   }}
                 >
-                  ksengur@firat.edu.tr
+                  Prof.Dr. Hüseyin Altun
                 </Typography>
-              </a>
-              <a href="tel:04242374343">
-                <BsFillTelephoneFill size={18} color="#fff" />
+                <a href="mailto:haltun@firat.edu.tr">
+                  <MdMail size={18} color="#fff" />
+                  <Typography
+                    sx={{
+                      fontWeight: "bold",
+                      color: "#fff",
+                      fontSize: ".75rem",
+                    }}
+                  >
+                    haltun@firat.edu.tr
+                  </Typography>
+                </a>
+                <a href="tel:04242374343">
+                  <BsFillTelephoneFill size={18} color="#fff" />
+                  <Typography
+                    sx={{
+                      fontWeight: "bold",
+                      color: "#fff",
+                      fontSize: ".75rem",
+                    }}
+                  >
+                    +90 0424 237 43 37
+                  </Typography>
+                </a>
                 <Typography
                   sx={{
                     fontWeight: "bold",
                     color: "#fff",
+                    textAlign: "center",
                     fontSize: ".75rem",
+                    padding: "5px 3px 5px 3px",
+                    marginTop: "8px",
+                    borderTop: "1px solid #ff5722",
+                    borderBottom: "1px solid #ff5722",
+                    width: "100%",
                   }}
                 >
-                  +90 0424 237 43 43
+                  Prof.Dr. Melih Cevdet İnce
                 </Typography>
-              </a>
-              <Typography
-                sx={{
-                  fontWeight: "bold",
-                  color: "#fff",
-                  textAlign: "center",
-                  fontSize: ".75rem",
-                  marginTop: "8px",
-                  padding: "5px 3px 5px 3px",
-                  borderBottom: "1px solid #ff5722",
-                  borderTop: "1px solid #ff5722",
-                  width: "100%",
-                }}
-              >
-                Prof.Dr. Hüseyin Altun
-              </Typography>
-              <a href="mailto:haltun@firat.edu.tr">
-                <MdMail size={18} color="#fff" />
+                <a href="mailto:mcince@gmail.com">
+                  <MdMail size={18} color="#fff" />
+                  <Typography
+                    sx={{
+                      fontWeight: "bold",
+                      color: "#fff",
+                      fontSize: ".75rem",
+                    }}
+                  >
+                    mcince@gmail.com
+                  </Typography>
+                </a>
                 <Typography
                   sx={{
+                    textAlign: "left",
+                    backgroundColor: "#232537",
+                    padding: "10px 15px",
                     fontWeight: "bold",
                     color: "#fff",
-                    fontSize: ".75rem",
+                    fontSize: "0.8rem",
+                    borderLeft: "5px solid #ff5722",
+                    marginTop: "1rem",
+                    fontFamily: "initial",
                   }}
                 >
-                  haltun@firat.edu.tr
+                  YABANCI DİL
                 </Typography>
-              </a>
-              <a href="tel:04242374343">
-                <BsFillTelephoneFill size={18} color="#fff" />
-                <Typography
-                  sx={{
-                    fontWeight: "bold",
-                    color: "#fff",
-                    fontSize: ".75rem",
-                  }}
-                >
-                  +90 0424 237 43 37
-                </Typography>
-              </a>
-              <Typography
-                sx={{
-                  fontWeight: "bold",
-                  color: "#fff",
-                  textAlign: "center",
-                  fontSize: ".75rem",
-                  padding: "5px 3px 5px 3px",
-                  marginTop: "8px",
-                  borderTop: "1px solid #ff5722",
-                  borderBottom: "1px solid #ff5722",
-                  width: "100%",
-                }}
-              >
-                Prof.Dr. Melih Cevdet İnce
-              </Typography>
-              <a href="mailto:mcince@gmail.com">
-                <MdMail size={18} color="#fff" />
-                <Typography
-                  sx={{
-                    fontWeight: "bold",
-                    color: "#fff",
-                    fontSize: ".75rem",
-                  }}
-                >
-                  mcince@gmail.com
-                </Typography>
-              </a>
-              <Typography
-                sx={{
-                  textAlign: "left",
-                  backgroundColor: "#232537",
-                  padding: "10px 15px",
-                  fontWeight: "bold",
-                  color: "#fff",
-                  fontSize: "0.8rem",
-                  borderLeft: "5px solid #ff5722",
-                  marginTop: "1rem",
-                  fontFamily: "initial",
-                }}
-              >
-                YABANCI DİL
-              </Typography>
-              <div className="language">
-                <span>İngilizce</span>
-                <Rating value={3} readOnly />
-              </div>
-            </Item>
+                <div className="language">
+                  <span>İngilizce</span>
+                  <Rating value={3} readOnly />
+                </div>
+              </Item>
+            </Grid>
+            <Details />
           </Grid>
-          <Details />
-        </Grid>
-      </Box>
-    </div>
+        </Box>
+      </div>
+    </>
   );
 });
